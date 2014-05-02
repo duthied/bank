ddc978542652d48db57857bfd2ddaa87f9f09912
Devlon Duthie <duthied@gmail.com>
2014-05-01 21:56:32 -0700

    Adds seed data

--- bank/db/seeds.rb
+++ bank/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+AdminUser.create! do |au|
+  au.email = 'admin@example.com'
+  au.password = au.password_confirmation = 'password'
+end
+
+User.create! do |u|
+  u.card_number = '123123123'
+  u.pin = '1234'
+end
+
+Account.create! do |a|
+  a.title = 'Checking'
+  a.balance = 123.45
+  a.user_id = User.first.id
+end
