62f0b94421d1d992048719beb5efbb0aa25dac0c
Devlon Duthie <duthied@gmail.com>
2014-05-01 18:55:31 -0700

    initial bank rails app

---
+++ bank/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bank::Application.config.secret_key_base = '8a0c10e046c6ba6341309bb3a28615c7ab0b983cc5a4794322d2ead4584adb14534ccd72288d7119d72247f61370780891d81f698bc203fafaef4de3b874b53f'
---
+++ bank/config/routes.rb
@@ -0,0 +1,3 @@
+Bank::Application.routes.draw do
+  
+end
