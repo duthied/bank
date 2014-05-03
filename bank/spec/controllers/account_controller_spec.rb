7b5d6bdbae7cb4cdc02a1c5559ad891d39f5a985
Devlon Duthie <duthied@gmail.com>
2014-05-02 21:53:32 -0700

    Pending controller spacs

--- bank/spec/controllers/account_controller_spec.rb
+++ bank/spec/controllers/account_controller_spec.rb
@@ -2,18 +2,22 @@ require 'spec_helper'
 
 describe AccountController do
 
-  describe "GET 'withdraw'" do
-    it "returns http success" do
-      get 'withdraw'
-      expect(response).to be_success
-    end
+  let(:user) { FactoryGirl.create(:user) }
+  let(:account) { FactoryGirl.create(:account, user: user, balance: 100) }
+
+  describe "POST 'withdraw'" do
+    pending "need to figure out how to correctly do this"
+    # it "returns success" do
+    #   expect(:post => "/api/account/#{account.id}/withdraw", :amount => 10).to be_success
+    # end
   end
 
   describe "GET 'balance'" do
-    it "returns http success" do
-      get 'balance'
-      expect(response).to be_success
-    end
+    pending "need to figure out how to correctly do this"
+    # it "returns http success" do
+    #   get "/api/account/#{account.id}/balance"
+    #   expect(response).to be_success
+    # end
   end
 
 end
