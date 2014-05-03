6c9f7d9b8c07395e191df3463770f0c87df63d94
Devlon Duthie <duthied@gmail.com>
2014-05-03 12:24:45 -0700

    Adds headers for submitting cardnumber and pin for requests

--- bank/app/controllers/account_controller.rb
+++ bank/app/controllers/account_controller.rb
@@ -1,9 +1,11 @@
 class AccountController < ApplicationController
   skip_before_filter  :verify_authenticity_token
+  before_filter :card_auth_check
 
   def withdraw
     account = find_account(params[:id])
     amount = params[:amount].to_f
+    
     @response = TransactionWithdraw.call(account, amount)
   end
 
@@ -15,6 +17,12 @@ class AccountController < ApplicationController
 
     def find_account(id)
       Account.find(id)
+    end
+
+    def card_auth_check
+      if User.find_by_card_number_and_pin(request.headers['X-Card-Number'], request.headers['X-Pin']).nil?
+        render :status => 403, :text => 'Forbidden'
+      end
     end
 
 end
